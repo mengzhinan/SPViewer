5c524c4ddf8353642508c9773e959f67a92a15be
duke <duke@zhihu.com>
2023-01-05 19:56:58 +0800

    init

---
+++ SPViewer/src/main/java/com/aaa/spviewer/model/FileContentItem.kt
@@ -0,0 +1,11 @@
+package com.aaa.spviewer.model
+
+/**
+ * @Author: duke
+ * @DateTime: 2023-01-05 15:29:05
+ * @Description: 功能描述：
+ */
+class FileContentItem {
+    var key: String? = null
+    var value: String? = null
+}
