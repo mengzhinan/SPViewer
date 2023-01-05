f1b712329d661fd06b95bccb9f7e8ec7b2071409
duke <duke@zhihu.com>
2023-01-05 14:07:51 +0800

    init

--- app/src/main/java/com/aaa/spviewer/MainActivity.kt
+++ app/src/main/java/com/aaa/spviewer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.aaa.spviewer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.zhihu.spviewer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
